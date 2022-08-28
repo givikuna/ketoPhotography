10ac6e22c78363cb95b2126cc864cece238a422c
givikuna <thommaster10@gmail.com>
2022-08-28 17:52:30 -0400

    Added globalPathFinder()
    I have added the globalPathFinder() function to index.js, this function can be used to get the path to any location from any location

--- cgi/index.js
+++ cgi/index.js
@@ -6,6 +6,31 @@ const url = require('url');
 
 const pathToGmailInfo = "../data/contactGmail.txt";
 
+function globalPathFinder(listOfFoldersToGoThrough, nameOfFile) {
+    try {
+        var currentPath = "";
+        for (var i = 0; i < listOfFoldersToGoThrough.length; i++) {
+            var folderCurrentlyBeingSearchedFor = listOfFoldersToGoThrough[i];
+            var pathToSearchTheExistanceOf = null;
+            if (currentPath == "") {
+                pathToSearchTheExistanceOf = "./" + folderCurrentlyBeingSearchedFor;
+            } else {
+                pathToSearchTheExistanceOf = currentPath + folderCurrentlyBeingSearchedFor;
+            }
+            if (fs.existsSync(pathToSearchTheExistanceOf)) {
+                currentPath = currentPath + folderCurrentlyBeingSearchedFor + "/";
+            } else {
+                i = i - 1;
+                currentPath = currentPath + "../";
+            }
+        }
+        return path.join(currentPath, nameOfFile);
+    } catch (error) {
+        console.log("index.js ERROR: " + error);
+        return "index.js ERROR: " + error;
+    }
+}
+
 app.get('/', function (req, res) {
     try {
         var infoFromURL = url.parse(req.url, true).query;
