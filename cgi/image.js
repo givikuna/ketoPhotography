b98942f8ce3ca6cdddf19edd96974c53691d94a5
givikuna <thommaster10@gmail.com>
2022-08-28 18:22:48 -0400

    Added globalPathFinder() function
    I have added globalPathFinder() which is a function that could find any required files using a path.

--- cgi/image.js
+++ cgi/image.js
@@ -3,6 +3,31 @@ const fs = require('fs');
 const url = require('url');
 const path = require('path');
 
+function globalPathFinder(listOfFoldersToGoThrough, nameOfFile) {
+    try {
+        var currentPath = "";
+        for (var i = 0; i < listOfFoldersToGoThrough.length; i++) {
+            var folderCurrentlyBeingSearchedFor = listOfFoldersToGoThrough[i];
+            var pathToSearchTheExistanceOf = null;
+            if (currentPath == "") {
+                pathToSearchTheExistanceOf = "./" + folderCurrentlyBeingSearchedFor;
+            } else {
+                pathToSearchTheExistanceOf = currentPath + folderCurrentlyBeingSearchedFor;
+            }
+            if (fs.existsSync(pathToSearchTheExistanceOf)) {
+                currentPath = currentPath + folderCurrentlyBeingSearchedFor + "/";
+            } else {
+                i = i - 1;
+                currentPath = currentPath + "../";
+            }
+        }
+        return path.join(currentPath, nameOfFile);
+    } catch (error) {
+        console.log("image.js globalPathFinder() ERROR: " + error);
+        return "image.js globalPathFinder() ERROR: " + error;
+    }
+}
+
 http.createServer(function (req, res) {
     try {
         var infoFromURL = url.parse(req.url, true).query;
