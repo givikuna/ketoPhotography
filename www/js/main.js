0f1483951a34bc2d8bc074ebe074d5b41cad1114
givikuna <thommaster10@gmail.com>
2022-08-29 12:57:37 -0400

    Finished the mainJS

--- www/js/main.js
+++ www/js/main.js
@@ -1,10 +1,19 @@
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////// - GLOBAL VARIABLES:
+
+
 const ketoContactGmail_G = "@ketoGmailINFORMATION"; //the gmail of Keto, used for alerts and so on
 const maingLang_G = "@lang"; //the language chosen by the user
 var chosenAlbumID_G = null; //the ID of the album chosen by the user
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////// - ERROR HANDLER FUNCTIONS:
+
 function askForHelp(info) {
 	//
 }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////// - ASSISTANT FUNCTIONS:
 
 function languageChooser(langInfo) {
 	if (langInfo == null || langInfo == undefined || langInfo == "" || !langInfo || typeof langInfo == "number") {
@@ -26,22 +35,6 @@ function checkLang(lang) {
 		return true;
 	}
 	return false;
-}
-
-function actualTranslator(neededLang, givenArr, otherInfo) {
-	if (neededLang == "rus") {
-		if (!givenArr[otherInfo].textRus) { } else {
-			return givenArr[otherInfo].textRus;
-		}
-	} else if (neededLang == "geo") {
-		if (!givenArr[otherInfo].textGeo) { } else {
-			return givenArr[otherInfo].textGeo;
-		}
-	} else {
-		if (!givenArr[otherInfo].textEng) { } else {
-			return givenArr[otherInfo].textEng;
-		}
-	}
 }
 
 function checkJob(job) {
@@ -68,6 +61,22 @@ function ketoTranslatorHelper_SecurityManager(b, c) {
 		return true;
 	}
 	return false;
+}
+
+function actualTranslator(neededLang, givenArr, otherInfo) {
+	if (neededLang == "rus") {
+		if (!givenArr[otherInfo].textRus) { } else {
+			return givenArr[otherInfo].textRus;
+		}
+	} else if (neededLang == "geo") {
+		if (!givenArr[otherInfo].textGeo) { } else {
+			return givenArr[otherInfo].textGeo;
+		}
+	} else {
+		if (!givenArr[otherInfo].textEng) { } else {
+			return givenArr[otherInfo].textEng;
+		}
+	}
 }
 
 function ketoTranslatorHelper(neededLang, givenArr, otherInfo) {
@@ -111,6 +120,9 @@ function serverDisconnectErr() {
 	alert("Server couldn\'t connect, try again later or contact us at " + ketoContactGmail_G);
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////// - MANAGEMENT FUNCTIONS:
+
+// loads the whole page
 function bodyOnloadFunc(contactedSiteInfo) {
 	var xhttp = new XMLHttpRequest(); //use to connect to the servers
 	var url = "@dynamicLink:8093/?type=mainPage";
@@ -136,16 +148,9 @@ function bodyOnloadFunc(contactedSiteInfo) {
 	xhttp.send();
 }
 
-function fillGalleries(headerTextArray, mainArray) {
-	var galleriesContent_DIV = document.getElementById('galleriesContent');
-	galleriesContent_DIV.innerHTML = "";
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////// - BUILDER FUNCTIONS:
 
-	for (var i = 0; i < mainArray.length; i++) {
-		galleriesContent_DIV.innerHTML = galleriesContent_DIV.innerHTML + "<a class=\"a2\" href=\"@dynamicLink:8091/?page=in_gallery&galleryID=" + mainArray[i].id + "&lang=" + maingLang_G + "\">" + ketoTranslator(maingLang_G, "translation_for_lang_names", i, mainArray) + "</a> ";
-	}
-	fillBlanks(maingLang_G, headerTextArray);
-}
-
+// fills the images in the galleries in
 function fillImagesInGalleries(galleryID, mainArray) {
 	var imagesOfTheGalleryDiv = document.getElementById('imagesOfTheGallery');
 	imagesOfTheGalleryDiv.innerHTML = "";
@@ -167,6 +172,18 @@ function fillImagesInGalleries(galleryID, mainArray) {
 	}
 }
 
+// fills the names of the galleries in the header
+function fillGalleries(headerTextArray, mainArray) {
+	var galleriesContent_DIV = document.getElementById('galleriesContent');
+	galleriesContent_DIV.innerHTML = "";
+
+	for (var i = 0; i < mainArray.length; i++) {
+		galleriesContent_DIV.innerHTML = galleriesContent_DIV.innerHTML + "<a class=\"a2\" href=\"@dynamicLink:8091/?page=in_gallery&galleryID=" + mainArray[i].id + "&lang=" + maingLang_G + "\">" + ketoTranslator(maingLang_G, "translation_for_lang_names", i, mainArray) + "</a> ";
+	}
+	fillBlanks(maingLang_G, headerTextArray);
+}
+
+// fills the portfolio section
 function fillPortfolioSection(mainArray) { //ISN'T TESTED VIA test.js
 	var viewMyWorkHomePageId_DIV = document.getElementById('viewMyWorkHomePageId');
 	viewMyWorkHomePageId_DIV.innerHTML = "";
@@ -178,10 +195,42 @@ function fillPortfolioSection(mainArray) { //ISN'T TESTED VIA test.js
 	}
 }
 
+// fills out the words in website in different languages (currently English, Russian and Georgian)
 function fillBlanks(language, headerTextArray) { //ISN'T TESTED VIA test.js
 	for (let i = 0; i < headerTextArray.length; i++) {
 		if (document.getElementById(headerTextArray[i].id) !== null) {
 			document.getElementById(headerTextArray[i].id).innerHTML = ketoTranslator(language, "translation_for_the_main_text", i, headerTextArray);
 		}
 	}
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////// - EXPORTING OF FUNCTIONS:
+
+// If we're running under Node
+/*
+	exports only exists if we're running main.js as node (which is when we're testing it), so when it is being ran as node it'll export
+	all the functions which can be tested.
+
+	If it is not running in node, or in other words probably not being tested, then it'll not run, as exports would be undefined, and in the
+	if condition, the functions are only exported when it is not undefined.
+*/
+if (typeof exports !== 'undefined') {
+	exports.serverDisconnectErr = serverDisconnectErr;
+
+	exports.ketoTranslatorHelper = ketoTranslatorHelper;
+	exports.checkOtherInfo = checkOtherInfo;
+	exports.checkJob = checkJob;
+	exports.languageChooser = languageChooser;
+	exports.checkLang = checkLang;
+	exports.askForHelp = askForHelp;
+	exports.ketoTranslator_SecurityManager = ketoTranslator_SecurityManager;
+	exports.ketoTranslator = ketoTranslator;
+	exports.ketoTranslatorHelper_SecurityManager = ketoTranslatorHelper_SecurityManager;
+	exports.actualTranslator = actualTranslator;
+
+	exports.fillBlanks = fillBlanks;
+	exports.fillPortfolioSection = fillPortfolioSection;
+	exports.fillGalleries = fillGalleries;
+	exports.fillImagesInGalleries = fillImagesInGalleries;
+	exports.bodyOnloadFunc = bodyOnloadFunc;
 }
