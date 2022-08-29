2a4e811c1158989ee075f68dbb905090a3d9bfc9
givikuna <thommaster10@gmail.com>
2022-08-29 12:49:18 -0400

    Added askForHelp()
    Added the function askForHelp(), which is to be used to ask for help to the devs, once there is an error noticed in the code.

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,7 @@
 const ketoContactGmail_G = "@ketoGmailINFORMATION"; //the gmail of Keto, used for alerts and so on
 const maingLang_G = "@lang"; //the language chosen by the user
 var chosenAlbumID_G = null; //the ID of the album chosen by the user
+
+function askForHelp(info) {
+	//
+}
