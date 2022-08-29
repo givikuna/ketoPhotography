670f2ff1ffcfe1884a016077df94e99fe0d60730
givikuna <thommaster10@gmail.com>
2022-08-29 17:13:47 -0400

    Tested actualTranslator()

--- cgi/tests/main.test.js
+++ cgi/tests/main.test.js
@@ -210,6 +210,302 @@ describe('main.js', () => {
             expect(atStub()).to.equal(true);
         });
     });
+
+    context('actualTranslator()', () => {
+        beforeEach(() => {
+            mainJS = rewire(globalPathFinder(["www", "js"], "main.js"));
+        });
+
+        afterEach(() => {
+            mainJS = rewire(globalPathFinder(["www", "js"], "main.js"));
+        });
+
+        it('sends \'(\"BLAH\", [{textEng: \"BLINN\"}], 0)\', expects \"BLINN\"', () => {
+            expect(mainJS.actualTranslator("BLAH", [{ textEng: "BLINN" }], 0)).to.equal("BLINN");
+        });
+
+        it('sends \'(\"rus\", [{textEng: 10}], 0)\' and expects failure', () => {
+            expect(mainJS.actualTranslator("rus", [{ textEng: 10 }], 0)).to.throw;
+        });
+
+        it('sends \'(\"rus\", [{textRus: 10}], 0)\' and expects 10', () => {
+            expect(mainJS.actualTranslator("rus", [{ textRus: 10 }], 0)).to.equal(10);
+        });
+
+        it('sends \'(\"geo\", [{textGeo: 10}], 0)\' and expects 10', () => {
+            expect(mainJS.actualTranslator("geo", [{ textGeo: 10 }], 0)).to.equal(10);
+        });
+    });
+
+    context('languageChooser()', () => {
+        beforeEach(() => {
+            mainJS = rewire(globalPathFinder(["www", "js"], "main.js"));
+        });
+
+        afterEach(() => {
+            mainJS = rewire(globalPathFinder(["www", "js"], "main.js"));
+        });
+
+        it('sends \"rus\" and other types of Russian names shouldn\'t return \"eng\", instead should return \"rus\"', () => {
+            const sendArr = [
+                "rus",
+                "russian",
+                "ruso",
+                "rusia",
+                "ruski",
+                "rusian",
+                "ru",
+                "rusuli",
+                "russ",
+                "russian language"
+            ];
+
+            for (var i = 0; i < sendArr.length; i++) {
+                expect(mainJS.languageChooser(sendArr[i])).to.not.equal("eng");
+                expect(mainJS.languageChooser(sendArr[i])).to.equal("rus");
+            }
+        });
+
+        it('sends \'(\"anglish\")\' and shouldn\'t return \"rus\" but instead should return \"eng\"', () => {
+            expect(mainJS.languageChooser("anglish")).to.not.equal("rus");
+            expect(mainJS.languageChooser("anglish")).to.equal("eng");
+        });
+
+        it('sends different nulls and errors and should return \"eng\"', () => {
+            const sendArr = [
+                null,
+                undefined,
+                0,
+                "ara qartuli",
+                12408702,
+                "@n6113h",
+                "3ng1i3h"
+            ];
+
+            for (var i = 0; i < sendArr.length; i++) {
+                expect(mainJS.languageChooser(sendArr[i])).to.equal("eng");
+            }
+        });
+
+        it('sends \"ქართული\" and other Georgian names and expects to return \"geo\"', () => {
+            const sendArr = [
+                "ქართული",
+                "geo",
+                "qartuli nana",
+                "cartuli nana",
+                "kartuli nana",
+                "kartluli",
+                "kartvelian language",
+                "kartuli ena",
+                "deda ena",
+                "qartuli ena",
+                "cartuli ena",
+                "geouri",
+                "gurjistani",
+                "georgiani",
+                "qartveli",
+                "georgianuri",
+                "gurjistan",
+                "georgian",
+                "kartveli",
+                "kutaisuri",
+                "kartuli",
+                "ka",
+                "kar",
+                "cartuli",
+                "cartveluri",
+                "cartvelian",
+                "qartveluri",
+                "qartvelian",
+                "kartvellian",
+                "kartvelian",
+                "qartuli",
+                "gorgian",
+                "ge"
+            ];
+
+            for (var i = 0; i < sendArr.length; i++) {
+                expect(mainJS.languageChooser(sendArr[i])).to.equal("geo");
+            }
+        });
+    });
+
+    context('checkLang()', () => {
+        var lStub;
+
+        beforeEach(() => {
+            lStub = sinon.stub(mainJS, 'languageChooser').returns("rus");
+
+            mainJS = rewire(globalPathFinder(["www", "js"], "main.js"));
+        });
+
+        afterEach(() => {
+            lStub.restore();
+
+            mainJS = rewire(globalPathFinder(["www", "js"], "main.js"));
+        });
+
+        it('sends \'(\"eng\")\', \'(\"rus\")\', and \'(\"geo\")\', while expecting lStub() to return "rus" and expects to return true for all', () => {
+            mainJS.__set__('languageChooser', lStub);
+
+            expect(mainJS.checkLang("eng")).to.equal(true);
+            expect(lStub).to.have.returned("rus");
+            expect(mainJS.checkLang("rus")).to.equal(true);
+            expect(lStub).to.have.returned("rus");
+            expect(mainJS.checkLang("geo")).to.equal(true);
+            expect(lStub).to.have.returned("rus");
+            expect(lStub.called).to.be.true;
+        });
+
+        it('sends \'(\"eng\")\', \'(\"rus\")\', and \'(\"geo\")\', while expecting lStub() to return "eng" and expects to return true for all', () => {
+            lStub = sinon.stub(mainJS, 'languageChooser').returns("eng");
+
+            mainJS.__set__('languageChooser', lStub);
+
+            expect(mainJS.checkLang("eng")).to.equal(true);
+            expect(lStub).to.have.returned("eng");
+            expect(mainJS.checkLang("rus")).to.equal(true);
+            expect(lStub).to.have.returned("eng");
+            expect(mainJS.checkLang("geo")).to.equal(true);
+            expect(lStub).to.have.returned("eng");
+            expect(lStub.called).to.be.true;
+        });
+
+        it('sends \'(\"eng\")\', \'(\"rus\")\', and \'(\"geo\")\', while expecting lStub() to return "geo", and expects to return true for all', () => {
+            lStub = sinon.stub(mainJS, 'languageChooser').returns("geo");
+
+            mainJS.__set__('languageChooser', lStub);
+
+            expect(mainJS.checkLang("eng")).to.equal(true);
+            expect(lStub).to.have.returned("geo");
+            expect(mainJS.checkLang("rus")).to.equal(true);
+            expect(lStub).to.have.returned("geo");
+            expect(mainJS.checkLang("geo")).to.equal(true);
+            expect(lStub).to.have.returned("geo");
+            expect(lStub.called).to.be.true;
+        });
+
+        it('should make lStub() return something but \"rus\", \"eng\" or \"geo\" and expects checkLang to return false', () => {
+            lStub = sinon.stub(mainJS, 'languageChooser').returns(false);
+
+            mainJS.__set__('languageChooser', lStub);
+
+            expect(mainJS.checkLang("eng")).to.equal(false);
+            expect(lStub.called).to.be.true;
+            expect(lStub).to.have.returned(false);
+            expect(lStub.callCount).be.be.at.least(3);
+        });
+    });
+
+    context('checkJob()', () => {
+        beforeEach(() => {
+            mainJS = rewire(globalPathFinder(["www", "js"], "main.js"));
+        });
+
+        afterEach(() => {
+            mainJS = rewire(globalPathFinder(["www", "js"], "main.js"));
+        });
+
+        it('sends \'(\"translation_for_lang_names\")\', expects checkJob() to return true', () => {
+            expect(mainJS.checkJob("translation_for_lang_names")).to.be.true;
+        });
+
+        it('sends \'(\"translation_for_the_main_text\")\', expects checkJob() to return true', () => {
+            expect(mainJS.checkJob("translation_for_the_main_text")).to.be.true;
+        });
+
+        it('sends \'(\"bruh\")\', expects checkJob() to return false', () => {
+            expect(mainJS.checkJob("bruh")).to.be.false;
+        });
+    });
+
+    context('checkOtherInfo()', () => {
+        beforeEach(() => {
+            mainJS = rewire(globalPathFinder(["www", "js"], "main.js"));
+        });
+
+        afterEach(() => {
+            mainJS = rewire(globalPathFinder(["www", "js"], "main.js"));
+        });
+
+        it('sends \'(null)\', expects it to be false', () => {
+            expect(mainJS.checkOtherInfo(null)).to.be.false;
+        });
+
+        it('sends \'(undefined)\', expects it to be false', () => {
+            expect(mainJS.checkOtherInfo(undefined)).to.be.false;
+        });
+
+        it('sends \'(\"StringAAAA\")\', expects it to be false', () => {
+            expect(mainJS.checkOtherInfo("StringAAAA")).to.be.false;
+        });
+
+        it('sends \'(-4)\', expects it to be false', () => {
+            expect(mainJS.checkOtherInfo(-4)).to.be.false;
+        });
+
+        it('sends \'(0)\', expects it to be true', () => {
+            expect(mainJS.checkOtherInfo(0)).to.be.true;
+        });
+
+        it('sends \'(567)\', expects it to be true', () => {
+            expect(mainJS.checkOtherInfo(567)).to.be.true;
+        });
+
+        it('sends \'(1.56)\', expects it to be false', () => {
+            expect(mainJS.checkOtherInfo(1.56)).to.be.false;
+        });
+    });
+
+    context('ketoTranslatorHelper_SecurityManager()', () => {
+        beforeEach(() => {
+            mainJS = rewire(globalPathFinder(["www", "js"], "main.js"));
+        });
+
+        afterEach(() => {
+            mainJS = rewire(globalPathFinder(["www", "js"], "main.js"));
+        });
+
+        it('sends \'(4, 5)\', and expects to be false', () => {
+            expect(mainJS.ketoTranslatorHelper_SecurityManager(4, 5)).to.be.false;
+        });
+
+        it('sends \'([], 5)\', and expects to be false', () => {
+            expect(mainJS.ketoTranslatorHelper_SecurityManager([], 5)).to.be.false;
+        });
+
+        it('sends \'([undefined], 5)\', and expects to be false', () => {
+            expect(mainJS.ketoTranslatorHelper_SecurityManager([undefined], 5)).to.be.false;
+        });
+
+        it('sends \'([null], 5)\', and expects to be false', () => {
+            expect(mainJS.ketoTranslatorHelper_SecurityManager([null], 5)).to.be.false;
+        });
+
+        it('sends \'([\"hi\", \"giiviieieerieu\", []], 5)\', and expects to be false', () => {
+            expect(mainJS.ketoTranslatorHelper_SecurityManager(["hi", "giiviieieerieu", []], 5)).to.be.false;
+        });
+
+        it('sends \'([\"hi\", \"giiviieieerieu\", null], 5)\', and expects to be false', () => {
+            expect(mainJS.ketoTranslatorHelper_SecurityManager(["hi", "giiviieieerieu", null], 5)).to.be.false;
+        });
+
+        it('sends \'([\"hi\", \"giiviieieerieu\", undefined], 5)\', and expects to be false', () => {
+            expect(mainJS.ketoTranslatorHelper_SecurityManager(["hi", "giiviieieerieu", undefined], 5)).to.be.false;
+        });
+
+        it('sends \'([\"hi\", \"oii\"], 5)\', and expects to be true', () => {
+            expect(mainJS.ketoTranslatorHelper_SecurityManager(["hi", "oii"], 5)).to.be.true;
+        });
+
+        it('sends \'(\"hi\", 5)\', and expects to be false', () => {
+            expect(mainJS.ketoTranslatorHelper_SecurityManager("hi", 5)).to.be.false;
+        });
+
+        it('sends \'([\"givi\"], 5)\', and expects to be true', () => {
+            expect(mainJS.ketoTranslatorHelper_SecurityManager(["givi"], 5)).to.be.true;
+        });
+    });
 });
 
 /*
