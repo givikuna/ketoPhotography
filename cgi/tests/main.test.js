1f286a36a04597d6339cab18eaf3a11e7db0dae6
givikuna <thommaster10@gmail.com>
2022-08-29 17:11:34 -0400

    Tested ketoTranslator()

--- cgi/tests/main.test.js
+++ cgi/tests/main.test.js
@@ -17,7 +17,6 @@ var mainJS = rewire(globalPathFinder(["www", "js"], "main.js"));
 // eval(fs.readFileSync(globalPathFinder(["www", "js"], "main.js")) + '');
 // console.log(mainJS);
 
-
 function globalPathFinder(listOfFoldersToGoThrough, nameOfFile) {
     var currentPath = "";
     for (var i = 0; i < listOfFoldersToGoThrough.length; i++) {
@@ -39,8 +38,60 @@ function globalPathFinder(listOfFoldersToGoThrough, nameOfFile) {
 }
 
 
+//
+
+
 describe('main.js', () => {
-    //
+    beforeEach(() => {
+        mainJS = rewire(globalPathFinder(["www", "js"], "main.js"));
+    });
+
+    afterEach(() => {
+        mainJS = rewire(globalPathFinder(["www", "js"], "main.js"));
+    });
+
+    context('ketoTranslator()', () => {
+        var smStub;
+        var hStub;
+
+        beforeEach(() => {
+            smStub = sinon.stub(mainJS, 'ketoTranslator_SecurityManager').returns(true);
+            hStub = sinon.stub(mainJS, 'ketoTranslatorHelper').returns(true);
+
+            mainJS = rewire(globalPathFinder(["www", "js"], "main.js"));
+        });
+
+        afterEach(() => {
+            smStub.restore();
+            hStub.restore();
+
+            mainJS = rewire(globalPathFinder(["www", "js"], "main.js"));
+        });
+
+        //actual it unit tests
+        it('sends \'(null, null, null, null)\' information and expects it to return an \"error\"', () => {
+            mainJS.__set__('ketoTranslator_SecurityManager', smStub);
+            mainJS.__set__('ketoTranslatorHelper', hStub);
+
+            expect(mainJS.ketoTranslator(null, null, null, null)).to.equal("error");
+            expect(smStub).to.have.been.calledOnce;
+            expect(hStub).to.not.have.been.called;
+            expect(smStub).to.have.returned(true);
+            expect(hStub()).to.be.true;
+        });
+
+        it('sends \'(null, \"translation_for_lang_names\", null, null)\', and expects it to return true', () => {
+            mainJS.__set__('ketoTranslator_SecurityManager', smStub);
+            mainJS.__set__('ketoTranslatorHelper', hStub);
+
+            expect(mainJS.ketoTranslator(null, "translation_for_lang_names", null, null)).to.equal(true);
+            expect(smStub.calledOnce).to.be.true;
+            expect(hStub.calledOnce).to.be.true;
+            expect(smStub).to.have.returned(true);
+            expect(hStub).to.have.returned(true);
+        });
+    });
+
 });
 
 /*
