00d61b3dea0a6dd4bc53a615da7b4f63463600d0
givikuna <thommaster10@gmail.com>
2022-08-29 17:10:49 -0400

    Added globalPathFinder()

--- cgi/tests/main.test.js
+++ cgi/tests/main.test.js
@@ -17,6 +17,28 @@ var mainJS = rewire(globalPathFinder(["www", "js"], "main.js"));
 // eval(fs.readFileSync(globalPathFinder(["www", "js"], "main.js")) + '');
 // console.log(mainJS);
 
+
+function globalPathFinder(listOfFoldersToGoThrough, nameOfFile) {
+    var currentPath = "";
+    for (var i = 0; i < listOfFoldersToGoThrough.length; i++) {
+        var folderCurrentlyBeingSearchedFor = listOfFoldersToGoThrough[i];
+        var pathToSearchTheExistanceOf = null;
+        if (currentPath == "") {
+            pathToSearchTheExistanceOf = "./" + folderCurrentlyBeingSearchedFor;
+        } else {
+            pathToSearchTheExistanceOf = currentPath + folderCurrentlyBeingSearchedFor;
+        }
+        if (fs.existsSync(pathToSearchTheExistanceOf)) {
+            currentPath = currentPath + folderCurrentlyBeingSearchedFor + "/";
+        } else {
+            i = i - 1;
+            currentPath = currentPath + "../";
+        }
+    }
+    return path.join(currentPath, nameOfFile);
+}
+
+
 describe('main.js', () => {
     //
 });
