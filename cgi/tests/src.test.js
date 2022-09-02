72e026c4fc565ac9500eb2dd9fac7dc11d70f898
givikuna <thommaster10@gmail.com>
2022-09-02 13:39:32 -0400

    Created the skeleton
    I have created the skeleton for the src.test.js

---
+++ cgi/tests/src.test.js
@@ -0,0 +1,68 @@
+const chai = require('chai');
+const expect = chai.expect;
+var should = require('chai').should()
+var assert = require('chai').assert
+const path = require('path');
+const fs = require('fs');
+const rewire = require('rewire');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+chai.use(sinonChai);
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+
+var sandbox = sinon.createSandbox();
+
+var srcJS = rewire("../src.js");
+
+function globalPathFinder(listOfFoldersToGoThrough, nameOfFile) {
+    var currentPath = "";
+    for (var i = 0; i < listOfFoldersToGoThrough.length; i++) {
+        var folderCurrentlyBeingSearchedFor = listOfFoldersToGoThrough[i];
+        var pathToSearchTheExistanceOf = null;
+        if (currentPath == "") {
+            pathToSearchTheExistanceOf = "./" + folderCurrentlyBeingSearchedFor;
+        } else {
+            pathToSearchTheExistanceOf = currentPath + folderCurrentlyBeingSearchedFor;
+        }
+        if (fs.existsSync(pathToSearchTheExistanceOf)) {
+            currentPath = currentPath + folderCurrentlyBeingSearchedFor + "/";
+        } else {
+            i = i - 1;
+            currentPath = currentPath + "../";
+        }
+    }
+    return path.join(currentPath, nameOfFile);
+}
+
+
+//
+
+
+describe('select.js', () => {
+    beforeEach(() => {
+        srcJS = rewire("../src.js");
+    });
+
+    afterEach(() => {
+        srcJS = rewire("../src.js");
+    });
+});
+
+/*
+
+    context('theFunc()', () => {
+        beforeEach(() => {
+            srcJS = rewire("../src.js");
+        });
+
+        afterEach(() => {
+            srcJS = rewire("../src.js");
+        });
+
+        it('sends __, but __, expects __', () => {
+            //
+        });
+    });
+
+*/
